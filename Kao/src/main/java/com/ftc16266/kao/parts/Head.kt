088a69df2b7cd2553b683245a4167b02a25fb0fe
Noah <noahbres@gmail.com>
2019-08-11 15:05:48 -0500

    refactor(Head): Make head extend BodyPart

--- Kao/src/main/java/com/ftc16266/kao/parts/Head.kt
+++ Kao/src/main/java/com/ftc16266/kao/parts/Head.kt
@@ -1,17 +1,16 @@
 package com.ftc16266.kao.parts
 
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 
-class Head(x: Float, y: Float, radius: Float, bgColor: Int) {
-    private var x = x
-    private var y = y
+class Head(x: Float, y: Float, radius: Float, bgColor: Int): BodyPart {
+    override var x = x
+    override var y = y
     private var radius = radius
 
     private var bgColor = bgColor
 
-    private val paint: Paint = Paint()
+    override val paint: Paint = Paint()
 
     init {
         paint.apply {
@@ -21,7 +20,7 @@ class Head(x: Float, y: Float, radius: Float, bgColor: Int) {
         }
     }
 
-    fun draw(canvas: Canvas) {
+    override fun draw(canvas: Canvas) {
         canvas.drawCircle(x, y, radius, paint)
     }
 }
