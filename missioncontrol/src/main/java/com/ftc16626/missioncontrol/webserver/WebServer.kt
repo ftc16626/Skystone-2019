664425e1bdff36df0d3d4ea16f16526aedde2954
Noah <noahbres@gmail.com>
2019-09-23 01:30:23 -0500

    feat(WebServer): add access-control headers to every webserver request

--- missioncontrol/src/main/java/com/ftc16626/missioncontrol/webserver/WebServer.kt
+++ missioncontrol/src/main/java/com/ftc16626/missioncontrol/webserver/WebServer.kt
@@ -23,31 +23,41 @@ class WebServer : NanoHTTPD(PORT) {
 
     override fun serve(session: IHTTPSession?): Response {
         val uri: String = session!!.uri
+        var response: Response? = null
 
         for(request in requestRESTList) {
             if(request.uri == uri && request.method == session.method) {
-                val (status, mimeType, response) = request.listener.onRequest()
-                return newFixedLengthResponse(status, mimeType, response)
+                val (status, mimeType, payload) = request.listener.onRequest()
+
+                response = newFixedLengthResponse(status, mimeType, payload)
             }
         }
 
-        return when (uri) {
-            "/" -> newFixedLengthResponse(
-                Response.Status.OK,
-                MIME_HTML,
-                "<html><body><h1>Mission Control Root</h1></body></html>"
-            )
-            "/base" -> newFixedLengthResponse(
-                Response.Status.OK,
-                MIME_HTML,
-                "<html><body><h1>Mission Control Base uri</h1></body></html>"
-            )
-            else -> newFixedLengthResponse(
-                Response.Status.OK,
-                MIME_HTML,
-                "<html><body><h1>Mission Control</h1></body></html>\""
-            )
+        if(response == null) {
+            response =  when (uri) {
+                "/" -> newFixedLengthResponse(
+                    Response.Status.OK,
+                    MIME_HTML,
+                    "<html><body><h1>Mission Control Root</h1></body></html>"
+                )
+                "/base" -> newFixedLengthResponse(
+                    Response.Status.OK,
+                    MIME_HTML,
+                    "<html><body><h1>Mission Control Base uri</h1></body></html>"
+                )
+                else -> newFixedLengthResponse(
+                    Response.Status.OK,
+                    MIME_HTML,
+                    "<html><body><h1>Mission Control</h1></body></html>\""
+                )
+            }
         }
+
+        response?.addHeader("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT")
+        response?.addHeader("Access-Control-Allow-Origin",  "*")
+        response?.addHeader("Access-Control-Allow-Headers", "X-Requested-With")
+
+        return response!!
     }
 
     fun registerRESTRequest(
