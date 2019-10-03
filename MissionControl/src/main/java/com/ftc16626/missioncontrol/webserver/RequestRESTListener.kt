77a9b938f433f02bdee232375017a0d640f00319
Noah <noahbres@gmail.com>
2019-10-03 02:37:17 -0500

    feat(RequestRESTListener): allow REST requests to specify/read the entire url

--- MissionControl/src/main/java/com/ftc16626/missioncontrol/webserver/RequestRESTListener.kt
+++ MissionControl/src/main/java/com/ftc16626/missioncontrol/webserver/RequestRESTListener.kt
@@ -1,5 +1,5 @@
 package com.ftc16626.missioncontrol.webserver
 
 interface RequestRESTListener {
-    fun onRequest(): RequestRESTResponse
+    fun onRequest(url: List<String>): RequestRESTResponse
 }
