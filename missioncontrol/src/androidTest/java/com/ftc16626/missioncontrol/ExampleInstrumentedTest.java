c58d0ef2aebad3aab91fb5d21644bfc98e9e2532
Noah <noahbres@gmail.com>
2019-10-03 14:49:30 -0500

    fix: purged all instance of 16266
    Because Mathew gave us the wrong team number

--- missioncontrol/src/androidTest/java/com/ftc16626/missioncontrol/ExampleInstrumentedTest.java
+++ missioncontrol/src/androidTest/java/com/ftc16626/missioncontrol/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getTargetContext();
 
-    assertEquals("com.ftc16266.missioncontrol.test", appContext.getPackageName());
+    assertEquals("com.ftc16626.missioncontrol.test", appContext.getPackageName());
   }
 }
