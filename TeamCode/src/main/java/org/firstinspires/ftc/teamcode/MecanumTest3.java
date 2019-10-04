906ca1776b86b227eb25bc7aa800eb6fa86f90b1
Noah <noahbres@gmail.com>
2019-10-03 23:40:55 -0500

    feat(MecanumTest4): create a mecanum test 4

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest3.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.hardware.MainHardware;
+
+// Left joystick is used to translate the bot
+// Right joystick controls the rotation of the bot
+@TeleOp(name="Mecanum Test 3", group="Mecanum")
+public class MecanumTest3 extends OpMode {
+
+  private MainHardware robot;
+
+  @Override
+  public void init() {
+    robot = new MainHardware(hardwareMap);
+
+    telemetry.addData("Status", "Initialized");
+  }
+
+  @Override
+  public void loop() {
+    double theta = Math.atan(gamepad1.left_stick_x + gamepad1.left_stick_y) + Math.PI / 4;
+    double magnitude = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+    double rot = gamepad1.right_stick_x;
+    magnitude = magnitude * (100 / 127) - rot;
+    double newX = Math.cos(theta) * magnitude;
+    double newY = Math.cos(theta) * magnitude;
+
+    final double motorPowerFrontLeft = radius * Math.cos(robotAngle) + rightX;
+    final double motorPowerFrontRight = radius * Math.sin(robotAngle) - rightX;
+    final double motorPowerBackLeft = radius * Math.sin(robotAngle) + rightX;
+    final double motorPowerBackRight = radius * Math.cos(robotAngle) - rightX;
+
+    robot.drive.motorFrontLeft.setPower(-motorPowerFrontLeft);
+    robot.drive.motorFrontRight.setPower(motorPowerFrontRight);
+    robot.drive.motorBackLeft.setPower(-motorPowerBackLeft);
+    robot.drive.motorBackRight.setPower(motorPowerBackRight);
+  }
+}
