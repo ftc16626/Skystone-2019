a7b63188b079f46723836fae25ec04842d17ab54
Noah <noahbres@gmail.com>
2019-10-03 22:49:51 -0500

    feat(MecanumDrive): create a class for the mecanum drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MecanumDrive.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import java.util.List;
+
+public class MecanumDrive {
+
+  public DcMotor motorFrontLeft = null;
+  public DcMotor motorFrontRight = null;
+  public DcMotor motorBackLeft = null;
+  public DcMotor motorBackRight = null;
+
+  public List<DcMotor> motorList;
+
+  public MecanumDrive(HardwareMap hwMap, String motorFrontLeftId, String motorFrontRightId,
+      String motorBackLeftId, String motorBackRightId, boolean runWithEncoders) {
+    motorFrontLeft = hwMap.get(DcMotor.class, motorFrontLeftId);
+    motorFrontRight = hwMap.get(DcMotor.class, motorFrontRightId);
+    motorBackLeft = hwMap.get(DcMotor.class, motorBackLeftId);
+    motorBackRight = hwMap.get(DcMotor.class, motorBackRightId);
+
+    motorList.add(motorFrontLeft);
+    motorList.add(motorFrontRight);
+    motorList.add(motorBackLeft);
+    motorList.add(motorBackRight);
+
+    stopMotors();
+
+    if (runWithEncoders) {
+      for (DcMotor motor : motorList) {
+        motor.setMode(RunMode.RUN_USING_ENCODER);
+      }
+    } else {
+      for (DcMotor motor : motorList) {
+        motor.setMode(RunMode.RUN_WITHOUT_ENCODER);
+      }
+    }
+  }
+
+  public void stopMotors() {
+    for (DcMotor motor : motorList) {
+      motor.setPower(0);
+    }
+  }
+}
