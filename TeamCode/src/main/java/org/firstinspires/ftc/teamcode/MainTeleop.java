dcdc6da39950d7e4204554053a757ae17ff4b680
Noah <noahbres@gmail.com>
2019-10-15 23:46:50 -0500

    feat(MainTeleop): add driver interface and GamepadListener to MainTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
@@ -2,29 +2,51 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventName;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventType;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadListener;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadType;
 import org.firstinspires.ftc.teamcode.hardware.MainHardware;
+import org.firstinspires.ftc.teamcode.gamepadextended.DriverInterface;
 
 @TeleOp(name = "Main TeleOp", group = "Mecanum")
-public class MainTeleop extends OpMode {
+public class MainTeleop extends OpMode implements GamepadListener {
 
   private MainHardware robot;
+  private DriverInterface driverInterface;
 
   @Override
   public void init() {
     robot = new MainHardware(hardwareMap);
+    driverInterface = new DriverInterface(gamepad1, gamepad2, this);
+
     telemetry.addData("Status", "Initialized");
   }
 
   @Override
   public void loop() {
-    double magnitude = Math.min(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y), 1);
-    double angle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-    double turn = gamepad1.right_stick_x;
+    driverInterface.update();
+
+    double magnitude = driverInterface.driver.getMagnitudeStrafeStick();
+    double angle = driverInterface.driver.getAngleTurnStick();
+    double turn = driverInterface.driver.getTurnStickX();
 
     robot.drive.setAngle(angle);
     robot.drive.setPower(magnitude);
     robot.drive.setTurn(turn);
 
     robot.update();
+  }
+
+  @Override
+  public void actionPerformed(GamepadEventName eventName, GamepadEventType eventType,
+      GamepadType gamepadType) {
+    if(eventType == GamepadEventType.BUTTON_PRESSED) {
+      switch(eventName) {
+        case GUIDE:
+          telemetry.addData("Toggle Me", "True");
+          break;
+      }
+    }
   }
 }
