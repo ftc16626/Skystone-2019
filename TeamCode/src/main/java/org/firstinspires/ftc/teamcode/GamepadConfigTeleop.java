5bdbd504379c58fb82697cde5c9448a76b640daf
Noah <noahbres@gmail.com>
2019-10-15 23:50:10 -0500

    feat(GamepadConfigTeleop): create a test teleop for gamepad config

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadConfigTeleop.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventName;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventType;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadListener;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadType;
+import org.firstinspires.ftc.teamcode.hardware.MainHardware;
+import org.firstinspires.ftc.teamcode.gamepadextended.DriverInterface;
+
+@TeleOp(name = "Gamepad Config TeleOp", group = "Testing")
+public class GamepadConfigTeleop extends OpMode implements GamepadListener {
+
+  private MainHardware robot;
+  private DriverInterface driverInterface;
+
+  @Override
+  public void init() {
+    robot = new MainHardware(hardwareMap);
+    driverInterface = new DriverInterface(gamepad1, gamepad2, this);
+
+    telemetry.addData("Status", "Initialized");
+  }
+
+  @Override
+  public void loop() {
+    driverInterface.update();
+
+    double magnitude = driverInterface.driver.getMagnitudeStrafeStick();
+    double angle = driverInterface.driver.getAngleTurnStick();
+    double turn = driverInterface.driver.getTurnStickX();
+
+    robot.drive.setAngle(angle);
+    robot.drive.setPower(magnitude);
+    robot.drive.setTurn(turn);
+
+    robot.update();
+  }
+
+  @Override
+  public void actionPerformed(GamepadEventName eventName, GamepadEventType eventType,
+      GamepadType gamepadType) {
+    if(eventType == GamepadEventType.BUTTON_PRESSED) {
+      switch(eventName) {
+        case GUIDE:
+          telemetry.addData("Toggle Me", "True");
+          break;
+      }
+    }
+  }
+}
