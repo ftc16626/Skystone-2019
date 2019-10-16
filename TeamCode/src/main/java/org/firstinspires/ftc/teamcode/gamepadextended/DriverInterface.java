6b915ac2c91706d2bc450dff0109a198cb610484
Noah <noahbres@gmail.com>
2019-10-15 23:46:28 -0500

    feat(DriverInterface): create a custom interface to handle gamepad control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepadextended/DriverInterface.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.gamepadextended;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadListener;
+
+public class DriverInterface {
+  public GamepadExtended driver;
+  public GamepadExtended aid;
+
+  public DriverInterface(Gamepad driver, Gamepad aid) {
+    this.driver = (GamepadExtended) driver;
+    this.aid = (GamepadExtended) aid;
+  }
+
+  public DriverInterface(Gamepad driver, Gamepad aid, GamepadListener listener) {
+    this.driver = (GamepadExtended) driver;
+    this.aid = (GamepadExtended) aid;
+
+    this.driver.setListener(listener);
+    this.aid.setListener(listener);
+  }
+
+  public void update() {
+    this.driver.update();
+    this.aid.update();
+  }
+
+  public void switchGamepads() {
+    GamepadExtended temp = driver;
+    this.driver = aid;
+    this.aid = temp;
+  }
+
+  public void setDriver(Gamepad gamepad) {
+    this.driver = (GamepadExtended) gamepad;
+  }
+
+  public void setDriver(GamepadExtended gamepadExtended) {
+    this.driver = gamepadExtended;
+  }
+
+  public void setAid(Gamepad gamepad) {
+    this.aid = (GamepadExtended) gamepad;
+  }
+
+  public void setAid(GamepadExtended gamepadExtended) {
+    this.aid = gamepadExtended;
+  }
+}
