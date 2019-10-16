c6e5440e2d0615f7ed71abffc0bec7ae8a31eb0d
Noah <noahbres@gmail.com>
2019-10-16 00:51:12 -0500

    fix(GamepadConfigTeleop): remove references of hardware in the test op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadConfigTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadConfigTeleop.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.gamepadextended.GamepadConfig;
+import org.firstinspires.ftc.teamcode.gamepadextended.GamepadConfig.StickControl;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventName;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventType;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadListener;
@@ -15,9 +17,18 @@ public class GamepadConfigTeleop extends OpMode implements GamepadListener {
   private MainHardware robot;
   private DriverInterface driverInterface;
 
+  private GamepadConfig enzoConfig = new GamepadConfig("Enzo's Config",
+      StickControl.STRAFE_LEFT_TURN_RIGHT_STICK, false, false, false, false);
+  private GamepadConfig mattConfig = new GamepadConfig("Matt's Config",
+      StickControl.STRAFE_RIGHT_TURN_LEFT_STICK, true, false, false, false);
+  private GamepadConfig emilioConfig = new GamepadConfig("Emilio's Config",
+      StickControl.STRAFE_LEFT_TURN_RIGHT_STICK, true, false, false, false);
+
+  private GamepadConfig[] configList = new GamepadConfig[]{enzoConfig, mattConfig, emilioConfig};
+  private int currentConfigPos = 0;
+
   @Override
   public void init() {
-    robot = new MainHardware(hardwareMap);
     driverInterface = new DriverInterface(gamepad1, gamepad2, this);
 
     telemetry.addData("Status", "Initialized");
@@ -26,26 +37,23 @@ public class GamepadConfigTeleop extends OpMode implements GamepadListener {
   @Override
   public void loop() {
     driverInterface.update();
-
-    double magnitude = driverInterface.driver.getMagnitudeStrafeStick();
-    double angle = driverInterface.driver.getAngleTurnStick();
-    double turn = driverInterface.driver.getTurnStickX();
-
-    robot.drive.setAngle(angle);
-    robot.drive.setPower(magnitude);
-    robot.drive.setTurn(turn);
-
-    robot.update();
+    telemetry.addData("StickX", driverInterface.driver.getStrafeStickX());
   }
 
   @Override
   public void actionPerformed(GamepadEventName eventName, GamepadEventType eventType,
       GamepadType gamepadType) {
-    if(eventType == GamepadEventType.BUTTON_PRESSED) {
-      switch(eventName) {
-        case GUIDE:
-          telemetry.addData("Toggle Me", "True");
-          break;
+    if (gamepadType == GamepadType.DRIVER && eventType == GamepadEventType.BUTTON_PRESSED) {
+      switch (eventName) {
+        case START:
+          currentConfigPos++;
+          if (currentConfigPos >= configList.length) {
+            currentConfigPos = 0;
+          }
+          driverInterface.driver.setConfig(configList[currentConfigPos]);
+
+          telemetry.addData("Current Config", configList[currentConfigPos].name);
+        break;
       }
     }
   }
