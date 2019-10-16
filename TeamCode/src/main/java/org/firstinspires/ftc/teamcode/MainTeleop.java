0a365a08e9f1d717043587ef3650e1e0f54f2701
Noah <noahbres@gmail.com>
2019-10-16 01:04:07 -0500

    feat(MainTeleop): implement configs in the main teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.gamepadextended.GamepadConfig;
+import org.firstinspires.ftc.teamcode.gamepadextended.GamepadConfig.StickControl;
+import org.firstinspires.ftc.teamcode.gamepadextended.StickResponseCurve;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventName;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventType;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadListener;
@@ -15,10 +18,33 @@ public class MainTeleop extends OpMode implements GamepadListener {
   private MainHardware robot;
   private DriverInterface driverInterface;
 
+  private GamepadConfig enzoConfig = new GamepadConfig(
+      "Enzo's Config",
+      StickControl.STRAFE_LEFT_TURN_RIGHT_STICK,
+      false, false,
+      false, false,
+      StickResponseCurve.CUBED);
+  private GamepadConfig mattConfig = new GamepadConfig(
+      "Matt's Config",
+      StickControl.STRAFE_RIGHT_TURN_LEFT_STICK,
+      true, false,
+      false, false,
+      StickResponseCurve.CUBED);
+  private GamepadConfig emilioConfig = new GamepadConfig(
+      "Emilio's Config",
+      StickControl.STRAFE_LEFT_TURN_RIGHT_STICK,
+      true, false,
+      false, false,
+      StickResponseCurve.CUBED);
+
+  private GamepadConfig[] configList = new GamepadConfig[]{enzoConfig, mattConfig, emilioConfig};
+  private int currentConfigPos = 0;
+
   @Override
   public void init() {
     robot = new MainHardware(hardwareMap);
     driverInterface = new DriverInterface(gamepad1, gamepad2, this);
+    driverInterface.driver.setConfig(configList[currentConfigPos]);
 
     telemetry.addData("Status", "Initialized");
   }
@@ -36,17 +62,30 @@ public class MainTeleop extends OpMode implements GamepadListener {
     robot.drive.setTurn(turn);
 
     robot.update();
+
+    telemetry.addData("Current Config", configList[currentConfigPos].name);
   }
 
   @Override
   public void actionPerformed(GamepadEventName eventName, GamepadEventType eventType,
       GamepadType gamepadType) {
-    if(eventType == GamepadEventType.BUTTON_PRESSED) {
-      switch(eventName) {
-        case GUIDE:
-          telemetry.addData("Toggle Me", "True");
-          break;
+    if (gamepadType == GamepadType.DRIVER) {
+      if (eventType == GamepadEventType.BUTTON_PRESSED) {
+        switch (eventName) {
+          case START:
+            incrementConfig();
+            break;
+        }
       }
     }
   }
+
+  private void incrementConfig() {
+    currentConfigPos++;
+    if (currentConfigPos >= configList.length) {
+      currentConfigPos = 0;
+    }
+    driverInterface.driver.setConfig(configList[currentConfigPos]);
+  }
+
 }
