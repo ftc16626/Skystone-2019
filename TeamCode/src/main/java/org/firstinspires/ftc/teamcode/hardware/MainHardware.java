d62b3dcccd961b94c317f53545548e0e7e51310d
Noah <noahbres@gmail.com>
2019-10-18 20:42:22 -0500

    feat(MainHardware): add the IMU to the MainHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MainHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MainHardware.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MainHardware {
 
   public MecanumDrive drive;
+  public RadicalIMU imu;
 
   private String[] motorIds = new String[]{
       "motorFrontLeft", "motorFrontRight",
@@ -13,6 +15,7 @@ public class MainHardware {
 
   public MainHardware(HardwareMap hwMap) {
     drive = new MecanumDrive(hwMap, motorIds[0], motorIds[1], motorIds[2], motorIds[3], false);
+    imu = new RadicalIMU(hwMap.get(BNO055IMU.class, "imu"));
   }
 
   public void init() {
@@ -21,5 +24,6 @@ public class MainHardware {
 
   public void update() {
     drive.update();
+    imu.update();
   }
 }
