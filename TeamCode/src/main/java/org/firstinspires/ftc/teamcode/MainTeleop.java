def1a6ad6af4f9661c42e0d219b1598e2b710c55
Noah <noahbres@gmail.com>
2019-10-25 12:01:20 -0500

    feat(MainTeleop): add driver aid features to the main teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
@@ -21,12 +21,14 @@ public class MainTeleop extends OpMode implements GamepadListener {
 
   @Override
   public void init() {
+    telemetry.addData("Status", "Initializing");
     missionControl = ((FtcRobotControllerActivity) hardwareMap.appContext).missionControl;
 
     robot = new MainHardware(hardwareMap);
     driverInterface = new DriverInterface(gamepad1, gamepad2, this);
     driverInterface.driver.setProfile(missionControl.getPilotProfileHandler().getCurrentProfile());
 
+    robot.init();
     telemetry.addData("Status", "Initialized");
   }
 
@@ -34,9 +36,44 @@ public class MainTeleop extends OpMode implements GamepadListener {
   public void loop() {
     driverInterface.update();
 
-    double magnitude = driverInterface.driver.getStrafeStickMagnitude();
-    double angle = driverInterface.driver.getStrafeStickAngle();
-    double turn = driverInterface.driver.getTurnStickX();
+    double magnitude = 0;
+    double angle = 0;
+    double turn = 0;
+
+    double realMag = driverInterface.driver.getStrafeStickMagnitude();
+    double realAngle = driverInterface.driver.getStrafeStickAngle();
+    double realTurn = driverInterface.driver.getTurnStickX();
+
+    // Ignore strafe stick if the turn stick is active
+    // Alternatively, integrate both turn and strafing if the right bumper is pressed
+    if (driverInterface.driver.gamepad.right_bumper) {
+      turn = realTurn;
+      magnitude = realMag;
+      angle = realAngle;
+    } else if (driverInterface.driver.getTurnStickX() != 0) {
+      turn = realTurn;
+    } else {
+      magnitude = realMag;
+      angle = realAngle;
+    }
+
+    // If left bumper is pressed, round the angles
+    if (driverInterface.driver.gamepad.left_bumper) {
+      double deg = Math.toDegrees(realAngle);
+      double rounded = Math.round(deg / 45) * 45;
+      angle = Math.toRadians(rounded);
+    }
+
+    // Divide speed in 2 if the 'b' button is pressed
+    if (driverInterface.driver.gamepad.b) {
+      if (magnitude != 0) {
+        magnitude /= 2;
+      }
+    }
+
+    if (driverInterface.driver.getProfile().enableFieldCentric) {
+      angle += Math.toRadians((robot.imu.getGlobalHeading() % 360));
+    }
 
     robot.drive.setAngle(angle);
     robot.drive.setPower(magnitude);
@@ -44,7 +81,7 @@ public class MainTeleop extends OpMode implements GamepadListener {
 
     robot.update();
 
-    telemetry.addData("Current Profile", driverInterface.driver.getProfile());
+    telemetry.addData("Current Profile", driverInterface.driver.getProfile().name);
   }
 
   @Override
