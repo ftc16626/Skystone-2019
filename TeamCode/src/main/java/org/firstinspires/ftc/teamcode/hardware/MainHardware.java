cb9f65f6a4058776a7064e1c6a0eeaf64182ac69
Noah <noahbres@gmail.com>
2019-10-25 11:59:39 -0500

    feat(MainHardware): add intake to MainHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MainHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MainHardware.java
@@ -7,19 +7,23 @@ public class MainHardware {
 
   public MecanumDrive drive;
   public RadicalIMU imu;
+  public Intake intake;
 
   private String[] motorIds = new String[]{
       "motorFrontLeft", "motorFrontRight",
       "motorBackLeft", "motorBackRight"
   };
 
+  private String[] intakeMotorIds = new String[] { "intakeLeft", "intakeRight" };
+
   public MainHardware(HardwareMap hwMap) {
-    drive = new MecanumDrive(hwMap, motorIds[0], motorIds[1], motorIds[2], motorIds[3], false);
+    drive = new MecanumDrive(hwMap, motorIds[0], motorIds[1], motorIds[2], motorIds[3], true);
     imu = new RadicalIMU(hwMap.get(BNO055IMU.class, "imu"));
+    intake = new Intake(hwMap, intakeMotorIds[0], intakeMotorIds[1]);
   }
 
   public void init() {
-//    hwMap =
+    drive.resetEncoders();
   }
 
   public void update() {
