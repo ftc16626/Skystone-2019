8189ba35ef3620735b2ac7c4a64a6651dd0f0c18
Noah <noahbres@gmail.com>
2019-10-29 22:53:02 -0500

    feat(Intake): change Intake logic
    Implement proper toggling. Change speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
@@ -8,18 +8,20 @@ public class Intake {
   private DcMotor motorLeft;
   private DcMotor motorRight;
 
-  private float power = 1;
+  private float power = -0.75f;
   private boolean on = false;
 
   public Intake(HardwareMap hwMap, String motorLeftId, String motorRightId) {
     motorLeft = hwMap.get(DcMotor.class, motorLeftId);
     motorRight = hwMap.get(DcMotor.class, motorRightId);
 
-    motorLeft.setDirection(Direction.REVERSE);
+    motorRight.setDirection(Direction.REVERSE);
   }
 
   public void setPower(float power) {
-    this.power = power;
+//    this.power = power;
+    motorLeft.setPower(power);
+    motorRight.setPower(power);
   }
 
   public void toggle() {
@@ -30,6 +32,18 @@ public class Intake {
 
   public void toggle(boolean on) {
     this.on = on;
+
+    if(this.on) {
+      motorLeft.setPower(power);
+      motorRight.setPower(power);
+    } else {
+      motorLeft.setPower(0);
+      motorRight.setPower(0);
+    }
+  }
+
+  public void reverse() {
+    power = power * -1;
 
     if(this.on) {
       motorLeft.setPower(power);
