f05edb4ea82df5d22daab77933cf8da3fa0a256e
Noah <noahbres@gmail.com>
2019-10-29 22:53:37 -0500

    feat(MainHardware): add StarboardServo to MainHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MainHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MainHardware.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class MainHardware {
 
   public MecanumDrive drive;
   public RadicalIMU imu;
   public Intake intake;
+  public Servo starboardServo;
 
   private String[] motorIds = new String[]{
       "motorFrontLeft", "motorFrontRight",
@@ -16,10 +18,14 @@ public class MainHardware {
 
   private String[] intakeMotorIds = new String[] { "intakeLeft", "intakeRight" };
 
+  private String starboardServoId = "starboardServo";
+
   public MainHardware(HardwareMap hwMap) {
     drive = new MecanumDrive(hwMap, motorIds[0], motorIds[1], motorIds[2], motorIds[3], true);
     imu = new RadicalIMU(hwMap.get(BNO055IMU.class, "imu"));
     intake = new Intake(hwMap, intakeMotorIds[0], intakeMotorIds[1]);
+    starboardServo = hwMap.get(Servo.class, starboardServoId);
+    starboardServo.scaleRange(0.35, 0.83);
   }
 
   public void init() {
