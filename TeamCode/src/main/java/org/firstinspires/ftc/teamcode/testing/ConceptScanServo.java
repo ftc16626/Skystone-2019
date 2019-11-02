22d25b949994804287e652cef3d2e92082143cf3
Noah <noahbres@gmail.com>
2019-11-01 22:00:30 -0500

    refactor(ConceptScanServo): move ConceptScanServo to testing group

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ConceptScanServo.java
@@ -47,7 +47,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name = "Concept: Scan Servo", group = "Concept")
+@TeleOp(name = "Scan Servo", group = "Testing")
 public class ConceptScanServo extends LinearOpMode {
 
   static final double INCREMENT = 0.01;     // amount to slew servo each CYCLE_MS cycle
