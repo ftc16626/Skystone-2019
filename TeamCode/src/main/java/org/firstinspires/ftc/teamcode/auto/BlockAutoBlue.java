dbdad3310920d6224d94a97a9b46bd9d0b08e3c7
Noah <noahbresler2002@gmail.com>
2019-11-22 20:58:14 -0600

    feat: adjustments for the block autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlockAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlockAutoBlue.java
@@ -6,18 +6,23 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.hardware.DriveBaseMecanum;
+import org.firstinspires.ftc.teamcode.hardware.DriveBaseMecanumOld;
 import org.firstinspires.ftc.teamcode.hardware.MainHardware;
 
 @Config
-@Autonomous(name="Auto - Block Auto Blue", group = "testing")
+@Autonomous(name = "Auto - Block Auto Blue", group = "testing")
 public class BlockAutoBlue extends LinearOpMode {
 
   public static double step1 = 650;
   public static double step2 = 150;
-  public static double step3 = 1570;
-  public static double step4 = step3 + 190;
-  public static double step5 = step1 - 160;
-
+  public static double step3 = 1600;
+  public static double step4 = step3 + 190 - 150;
+  public static double step5 = step2 + 110;
+  public static double step6 = step3 + 220;
+  //  public static double step7 = 250;
+  public static double step7 = 1120 + 80;
+  public static double step8 = 280 - 50;
+  public static double step9 = 1500;
 
   //  public static double turn1 = Math.toRadians(-90);
   MainHardware robot;
@@ -27,14 +32,23 @@ public class BlockAutoBlue extends LinearOpMode {
   @Override
   public void runOpMode() {
     robot = new MainHardware(hardwareMap);
-    DriveBaseMecanum drive = new DriveBaseMecanum(hardwareMap);
+    DriveBaseMecanumOld drive = new DriveBaseMecanumOld(hardwareMap);
 
     Trajectory trajectory1 = drive.trajectoryBuilder()
         .back(step1).build();
     Trajectory trajectory2 = drive.trajectoryBuilder().forward(step2).strafeRight(step3).build();
     Trajectory trajectory3 = drive.trajectoryBuilder().strafeLeft(step4).back(step5).build();
+    Trajectory trajectory4 = drive.trajectoryBuilder().forward(step2).strafeRight(step6).build();
+//    Trajectory trajectory5 = drive.trajectoryBuilder().strafeRight(step7).build();
+    Trajectory trajectory5 = drive.trajectoryBuilder().strafeLeft(step7).back(step8).build();
+    Trajectory trajectory6 = drive.trajectoryBuilder().strafeLeft(step2).build();
+    Trajectory trajectory7 = drive.trajectoryBuilder().back(step9).build();
+    Trajectory trajectory8 = drive.trajectoryBuilder().forward(step9 - 780).build();
 
-    robot.backRightServo.setPosition(1);
+//    robot.backRightServo.setPosition(1);
+//    robot.raiseBackRightServo();
+    robot.backRightServo.setPosition(0.55);
+
     robot.swingyServo.setPosition(1);
 
     double startingAngle = drive.getRawExternalHeading();
@@ -46,11 +60,12 @@ public class BlockAutoBlue extends LinearOpMode {
     }
 
     robot.init();
-    robot.swingyServo.setPosition(1);
 
     drive.followTrajectorySync(trajectory1);
 
-    robot.backRightServo.setPosition(0);
+//    robot.backRightServo.setPosition(0);
+    robot.perpendicularBackRightServo();
+    robot.swingyServo.setPosition(1);
 
     runtime.reset();
     while (runtime.seconds() < 0.8) {
@@ -59,18 +74,51 @@ public class BlockAutoBlue extends LinearOpMode {
 
     drive.followTrajectorySync(trajectory2);
 
-    robot.backRightServo.setPosition(1);
-    telemetry.addData("Heading", drive.getRawExternalHeading());
+//    robot.raiseBackRightServo();
+    robot.backRightServo.setPosition(0.55);
 
-    drive.turnSync(drive.getRawExternalHeading() - startingAngle);
+
+    drive.turnSync(-(drive.getRawExternalHeading() - startingAngle));
     drive.followTrajectorySync(trajectory3);
 
-    robot.backRightServo.setPosition(0);
+    robot.perpendicularBackRightServo();
 
     runtime.reset();
     while (runtime.seconds() < 0.8) {
-      ;
+
     }
+
+    drive.followTrajectorySync(trajectory4);
+
+//    robot.raiseBackRightServo();
+    robot.backRightServo.setPosition(0.55);
+    drive.turnSync(-(drive.getRawExternalHeading() - startingAngle));
+
+    runtime.reset();
+    while (runtime.seconds() < 0.8) {
+    }
+
+    drive.followTrajectorySync(trajectory5);
+
+    robot.perpendicularBackRightServo();
+
+    runtime.reset();
+    while (runtime.seconds() < 0.8) {
+    }
+
+    drive.followTrajectorySync(trajectory6);
+
+    drive.turnSync(Math.toRadians(-90));
+
+    drive.followTrajectorySync(trajectory7);
+
+    robot.backRightServo.setPosition(0.55);
+
+    runtime.reset();
+    while (runtime.seconds() < 0.2) {
+    }
+
+    drive.followTrajectorySync(trajectory8);
 
 //    drive.turnSync(turn1);
 //    drive.followTrajectorySync(trajectory2);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlockAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlockAutoRed.java
@@ -6,22 +6,23 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.hardware.DriveBaseMecanum;
+import org.firstinspires.ftc.teamcode.hardware.DriveBaseMecanumOld;
 import org.firstinspires.ftc.teamcode.hardware.MainHardware;
 
 @Config
-@Autonomous(name="Auto - Block Auto Red", group = "testing")
+@Autonomous(name = "Auto - Block Auto Red", group = "testing")
 public class BlockAutoRed extends LinearOpMode {
 
   public static double step1 = 650;
   public static double step2 = 150;
   public static double step3 = 1600;
-  public static double step4 = step3 + 190 - 120;
-  public static double step5 = step2 - 70;
+  public static double step4 = step3 + 190 - 150;
+  public static double step5 = step2 + 90;
   public static double step6 = step3 + 220;
-//  public static double step7 = 250;
-  public static double step7 = 1120;
-  public static double step8 = 280;
-  public static double step9 = 600;
+  //  public static double step7 = 250;
+  public static double step7 = 1120 + 80;
+  public static double step8 = 280 - 50;
+  public static double step9 = 1500;
 
   //  public static double turn1 = Math.toRadians(-90);
   MainHardware robot;
@@ -31,7 +32,7 @@ public class BlockAutoRed extends LinearOpMode {
   @Override
   public void runOpMode() {
     robot = new MainHardware(hardwareMap);
-    DriveBaseMecanum drive = new DriveBaseMecanum(hardwareMap);
+    DriveBaseMecanumOld drive = new DriveBaseMecanumOld(hardwareMap);
 
     Trajectory trajectory1 = drive.trajectoryBuilder()
         .back(step1).build();
@@ -42,8 +43,12 @@ public class BlockAutoRed extends LinearOpMode {
     Trajectory trajectory5 = drive.trajectoryBuilder().strafeRight(step7).back(step8).build();
     Trajectory trajectory6 = drive.trajectoryBuilder().strafeRight(step2).build();
     Trajectory trajectory7 = drive.trajectoryBuilder().back(step9).build();
+    Trajectory trajectory8 = drive.trajectoryBuilder().forward(step9 - 780).build();
 
-    robot.backRightServo.setPosition(1);
+//    robot.backRightServo.setPosition(1);
+//    robot.raiseBackRightServo();
+    robot.backRightServo.setPosition(0.55);
+
     robot.swingyServo.setPosition(1);
 
     double startingAngle = drive.getRawExternalHeading();
@@ -58,7 +63,8 @@ public class BlockAutoRed extends LinearOpMode {
 
     drive.followTrajectorySync(trajectory1);
 
-    robot.backRightServo.setPosition(0);
+//    robot.backRightServo.setPosition(0);
+    robot.perpendicularBackRightServo();
     robot.swingyServo.setPosition(1);
 
     runtime.reset();
@@ -68,12 +74,14 @@ public class BlockAutoRed extends LinearOpMode {
 
     drive.followTrajectorySync(trajectory2);
 
-    robot.backRightServo.setPosition(1);
+//    robot.raiseBackRightServo();
+    robot.backRightServo.setPosition(0.55);
+
 
     drive.turnSync(-(drive.getRawExternalHeading() - startingAngle));
     drive.followTrajectorySync(trajectory3);
 
-    robot.backRightServo.setPosition(0);
+    robot.perpendicularBackRightServo();
 
     runtime.reset();
     while (runtime.seconds() < 0.8) {
@@ -82,7 +90,8 @@ public class BlockAutoRed extends LinearOpMode {
 
     drive.followTrajectorySync(trajectory4);
 
-    robot.backRightServo.setPosition(1);
+//    robot.raiseBackRightServo();
+    robot.backRightServo.setPosition(0.55);
     drive.turnSync(-(drive.getRawExternalHeading() - startingAngle));
 
     runtime.reset();
@@ -91,7 +100,7 @@ public class BlockAutoRed extends LinearOpMode {
 
     drive.followTrajectorySync(trajectory5);
 
-    robot.backRightServo.setPosition(0);
+    robot.perpendicularBackRightServo();
 
     runtime.reset();
     while (runtime.seconds() < 0.8) {
@@ -102,6 +111,14 @@ public class BlockAutoRed extends LinearOpMode {
     drive.turnSync(Math.toRadians(-90));
 
     drive.followTrajectorySync(trajectory7);
+
+    robot.backRightServo.setPosition(0.55);
+
+    runtime.reset();
+    while (runtime.seconds() < 0.2) {
+    }
+
+    drive.followTrajectorySync(trajectory8);
 
 //    drive.turnSync(turn1);
 //    drive.followTrajectorySync(trajectory2);
