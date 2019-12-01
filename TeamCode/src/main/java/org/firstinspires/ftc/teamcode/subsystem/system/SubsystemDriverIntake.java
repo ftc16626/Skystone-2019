2da52fe17ef622617cbb421b8cf0327029a2ba15
Noah <noahbresler2002@gmail.com>
2019-12-01 17:23:19 -0600

    feat(SubsystemDriverIntake): move intake logic to a state machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/system/SubsystemDriverIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/system/SubsystemDriverIntake.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystem.system;
 
+import com.ftc16626.missioncontrol.util.statemachine.State;
+import com.ftc16626.missioncontrol.util.statemachine.StateMachine;
+import java.util.Objects;
 import org.firstinspires.ftc.teamcode.gamepadextended.DriverInterface;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventName;
 import org.firstinspires.ftc.teamcode.gamepadextended.listener.GamepadEventType;
@@ -14,7 +17,9 @@ public class SubsystemDriverIntake extends Subsystem implements GamepadListener 
 
   DriverInterface driverInterface;
 
-  public SubsystemDriverIntake(
+  private StateMachine stateMachine = buildStateMachine();
+
+  SubsystemDriverIntake(
       @NotNull MainHardware robot,
       @NotNull RadicalOpMode opMode,
       @NotNull DriverInterface driverInterface) {
@@ -25,7 +30,7 @@ public class SubsystemDriverIntake extends Subsystem implements GamepadListener 
 
   @Override
   public void update() {
-    if(driverInterface.aid.gamepad.b) {
+    if (driverInterface.aid.gamepad.b) {
       getRobot().intake.open();
     } else {
       getRobot().intake.close();
@@ -39,30 +44,82 @@ public class SubsystemDriverIntake extends Subsystem implements GamepadListener 
       if (eventType == GamepadEventType.BUTTON_PRESSED) {
         switch (eventName) {
           case LEFT_BUMPER:
-            if (!getRobot().intake.isMotorOn) {
-              getRobot().intake.directionBackward();
-              getRobot().intake.toggle(true);
-            } else if (getRobot().intake.power > 1) {
-              getRobot().intake.directionBackward();
-            } else {
-              getRobot().intake.toggle(false);
+            switch ((MyState) Objects.requireNonNull(stateMachine.getCurrentState())) {
+              case OFF:
+              case SPINNING_OUT:
+                stateMachine.transition(Transition.SPIN_IN);
+                break;
+              case SPINNING_IN:
+                stateMachine.transition(Transition.TURN_OFF);
+                break;
             }
-
             break;
           case RIGHT_BUMPER:
-            if (!getRobot().intake.isMotorOn) {
-              getRobot().intake.directionForward();
-              getRobot().intake.toggle(true);
-            } else if (getRobot().intake.power < 1) {
-              getRobot().intake.directionForward();
-            } else {
-              getRobot().intake.toggle(false);
+            switch ((MyState) Objects.requireNonNull(stateMachine.getCurrentState())) {
+              case OFF:
+              case SPINNING_IN:
+                stateMachine.transition(Transition.SPIN_OUT);
+                break;
+              case SPINNING_OUT:
+                stateMachine.transition(Transition.TURN_OFF);
+                break;
             }
-
-            getRobot().intake.directionForward();
             break;
         }
+
+        setMotorPowers();
       }
     }
+  }
+
+  private void setMotorPowers() {
+    switch ((MyState) Objects.requireNonNull(stateMachine.getCurrentState())) {
+      case OFF:
+        getRobot().intake.toggle(false);
+        break;
+      case SPINNING_IN:
+        getRobot().intake.toggle(true);
+        getRobot().intake.directionBackward();
+        break;
+      case SPINNING_OUT:
+        getRobot().intake.toggle(true);
+        getRobot().intake.directionForward();
+        break;
+    }
+  }
+
+  private StateMachine buildStateMachine() {
+    StateMachine stateMachine = new StateMachine();
+
+    stateMachine
+        .state(
+            new State(MyState.OFF)
+                .on(Transition.SPIN_IN, MyState.SPINNING_IN)
+                .on(Transition.SPIN_OUT, MyState.SPINNING_OUT)
+        )
+        .state(
+            new State(MyState.SPINNING_IN)
+                .on(Transition.TURN_OFF, MyState.OFF)
+                .on(Transition.SPIN_OUT, MyState.SPINNING_OUT)
+        )
+        .state(
+            new State(MyState.SPINNING_OUT)
+                .on(Transition.TURN_OFF, MyState.OFF)
+                .on(Transition.SPIN_IN, MyState.SPINNING_IN)
+        );
+
+    return stateMachine;
+  }
+
+  enum MyState {
+    OFF,
+    SPINNING_IN,
+    SPINNING_OUT
+  }
+
+  enum Transition {
+    SPIN_OUT,
+    SPIN_IN,
+    TURN_OFF
   }
 }
