e34da5041ad437986587b8061958c526bb507932
Noah <noahbresler2002@gmail.com>
2019-12-08 16:00:57 -0600

    feat(TurnTest): change turn test to turn 180 deg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TurnTest.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.hardware.DriveBaseMecanum;
 @Config
 @Autonomous(name="Tuning - Turn Test", group = "tuning")
 public class TurnTest extends LinearOpMode {
-  public static double ANGLE = 90; // deg
+  public static double ANGLE = 180; // deg
 
   @Override
   public void runOpMode() throws InterruptedException {
