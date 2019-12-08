7f617f26fbe67976f72d614d958460c8dfce0311
Noah <noahbresler2002@gmail.com>
2019-12-08 16:05:19 -0600

    feat(DriveConstants): fix drive constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveConstants.java
@@ -11,6 +11,7 @@ public class DriveConstants {
       GoBILDA5202Series.class);
 
   public static final boolean RUN_USING_ENCODER = true;
+//  public static final PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(getMotorVelocityF() * .1, getMotorVelocityF() * .1 * .1,0);
   public static final PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(20,4,12);
 
   public static double WHEEL_RADIUS = 50;
@@ -18,14 +19,16 @@ public class DriveConstants {
   // 99.5 is the assumed gear ration but 26.1 is the gobilda one. adjust so
 //  public static double GEAR_RATIO = 1 / ((99.5 / 26.1) / 2);
   public static double GEAR_RATIO = 0.5;
-  public static double TRACK_WIDTH = 383.89;
+  public static double TRACK_WIDTH = 423.33; //371.93,392.55,394.71,395.51,396.71,397.77,423.33
 
   public static double kV = 1.0 / rpmToVelocity(getMaxRpmGoBilda435());
+//  public static double kV = 0.00993;
   public static double kA = 0;
   public static double kStatic = 0;
+//  public static double kStatic = 0.02752;
 
   public static DriveConstraints BASE_CONSTRAINTS = new DriveConstraints(
-      762.0, 762.0, 0.0,
+      850, 762.0, 0.0,
       Math.toRadians(180.0), Math.toRadians(180.0), 0.0
   );
 
@@ -48,7 +51,8 @@ public class DriveConstants {
 
   public static double getTicksPerSec() {
     // note: MotorConfigurationType#getAchieveableMaxTicksPerSecond() isn't quite what we want
-    return (MOTOR_CONFIG.getMaxRPM() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
+    return (getMaxRpmGoBilda435() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
+//    return (MOTOR_CONFIG.getMaxRPM() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
   }
 
   public static double getMotorVelocityF() {
