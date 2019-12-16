48c579ef342778dd330086701038c0fb1c11e67a
Noah <noahbresler2002@gmail.com>
2019-12-15 19:24:09 -0600

    feat(DriveConstants): update custom encoder counts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveConstants.java
@@ -1,25 +1,36 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints;
 import com.qualcomm.hardware.motors.GoBILDA5202Series;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
+@Config
 public class DriveConstants {
 
   private static final MotorConfigurationType MOTOR_CONFIG = MotorConfigurationType.getMotorType(
       GoBILDA5202Series.class);
 
   public static final boolean RUN_USING_ENCODER = true;
+
+  public static double kP = 20;
+  public static double kI = 4;
+  public static double kD = 12;
+
 //  public static final PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(getMotorVelocityF() * .1, getMotorVelocityF() * .1 * .1,0);
-  public static final PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(20,4,12);
+  public static final PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(kP,kI,kD);
 
   public static double WHEEL_RADIUS = 50;
   // output (wheel) speed / input (motor) speed
   // 99.5 is the assumed gear ration but 26.1 is the gobilda one. adjust so
 //  public static double GEAR_RATIO = 1 / ((99.5 / 26.1) / 2);
+//  public static double GEAR_RATIO = 1;
   public static double GEAR_RATIO = 0.5;
-  public static double TRACK_WIDTH = 423.33; //371.93,392.55,394.71,395.51,396.71,397.77,423.33
+//  public static double TRACK_WIDTH = 423.33; //371.93,392.55,394.71,395.51,396.71,397.77,423.33
+  public static double TRACK_WIDTH = 384; //371.93,392.55,394.71,395.51,396.71,397.77,423.33
+//  public static double TRACK_WIDTH = 381; //371.93,392.55,394.71,395.51,396.71,397.77,423.33
+//    public static double TRACK_WIDTH = 360;
 
   public static double kV = 1.0 / rpmToVelocity(getMaxRpmGoBilda435());
 //  public static double kV = 0.00993;
@@ -27,13 +38,15 @@ public class DriveConstants {
   public static double kStatic = 0;
 //  public static double kStatic = 0.02752;
 
+  public static double ENCODER_COUNTS_PER_REV = 386.3;
+
   public static DriveConstraints BASE_CONSTRAINTS = new DriveConstraints(
       850, 762.0, 0.0,
       Math.toRadians(180.0), Math.toRadians(180.0), 0.0
   );
 
   public static double encoderTicksToInches(double ticks) {
-    return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / MOTOR_CONFIG.getTicksPerRev();
+    return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / ENCODER_COUNTS_PER_REV;
   }
 
   public static double rpmToVelocity(double rpm) {
@@ -51,7 +64,7 @@ public class DriveConstants {
 
   public static double getTicksPerSec() {
     // note: MotorConfigurationType#getAchieveableMaxTicksPerSecond() isn't quite what we want
-    return (getMaxRpmGoBilda435() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
+    return (getMaxRpmGoBilda435() * ENCODER_COUNTS_PER_REV / 60.0);
 //    return (MOTOR_CONFIG.getMaxRPM() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
   }
 
