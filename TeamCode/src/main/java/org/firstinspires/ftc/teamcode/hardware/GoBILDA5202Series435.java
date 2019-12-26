e85989675126596ff46a602d3ff7c538986ddaa4
Noah <noahbresler2002@gmail.com>
2019-12-26 02:22:16 -0600

    feat(GoBILDA5202Series435): add motor profile for 435 rpm gobilda motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/GoBILDA5202Series435.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.configuration.DistributorInfo;
+import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
+import com.qualcomm.robotcore.hardware.configuration.annotations.ExpansionHubPIDFPositionParams;
+import com.qualcomm.robotcore.hardware.configuration.annotations.ExpansionHubPIDFVelocityParams;
+import com.qualcomm.robotcore.hardware.configuration.annotations.MotorType;
+import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
+
+@MotorType(ticksPerRev = 386.3, gearing = 99.5, maxRPM = 435, orientation = Rotation.CCW)
+@DeviceProperties(xmlTag="goBILDA5202Series435Motor", name="GoBILDA 5202 series 435", builtIn = false)
+@DistributorInfo(distributor="goBILDA_distributor", model="goBILDA-5202", url="https://www.gobilda.com/5202-series-yellow-jacket-planetary-gear-motors/")
+@ExpansionHubPIDFVelocityParams(P=2.0, I=0.5, F=11.1)
+@ExpansionHubPIDFPositionParams(P=5.0)
+public class GoBILDA5202Series435 {
+
+}
