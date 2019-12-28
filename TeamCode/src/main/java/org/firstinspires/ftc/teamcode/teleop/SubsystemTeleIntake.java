1d7b35fa9bd9505305d950f2913e0cfa29cffa5b
Noah <noahbresler2002@gmail.com>
2019-12-28 07:35:18 -0600

    feat(SubsystemTeleIntake): update state machine to explicit type checking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SubsystemTeleIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SubsystemTeleIntake.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.teamcode.util.gamepadextended.listener.GamepadType;
 public class SubsystemTeleIntake extends Subsystem implements GamepadListener {
   private final Robot robot;
 
-  private StateMachine stateMachine = buildStateMachine();
+  private StateMachine<MyState, Transition> stateMachine = buildStateMachine();
 
   public SubsystemTeleIntake(Robot robot, DriverInterface driverInterface) {
     driverInterface.addListener(this);
@@ -28,7 +28,7 @@ public class SubsystemTeleIntake extends Subsystem implements GamepadListener {
     if (gamepadType == GamepadType.AID && eventType == GamepadEventType.BUTTON_PRESSED) {
       switch (eventName) {
         case LEFT_BUMPER:
-          switch ((MyState) Objects.requireNonNull(stateMachine.getCurrentState())) {
+          switch (Objects.requireNonNull(stateMachine.getCurrentState())) {
             case OFF:
             case SPINNING_OUT:
               stateMachine.transition(Transition.SPIN_IN);
@@ -39,7 +39,7 @@ public class SubsystemTeleIntake extends Subsystem implements GamepadListener {
           }
           break;
         case RIGHT_BUMPER:
-          switch ((MyState) Objects.requireNonNull(stateMachine.getCurrentState())) {
+          switch (Objects.requireNonNull(stateMachine.getCurrentState())) {
             case OFF:
             case SPINNING_IN:
               stateMachine.transition(Transition.SPIN_OUT);
@@ -56,7 +56,7 @@ public class SubsystemTeleIntake extends Subsystem implements GamepadListener {
   }
 
   private void setMotorPowers() {
-    switch ((MyState) Objects.requireNonNull(stateMachine.getCurrentState())) {
+    switch (Objects.requireNonNull(stateMachine.getCurrentState())) {
       case OFF:
         robot.subsystemIntake.setMotorOn(false);
         break;
@@ -71,22 +71,22 @@ public class SubsystemTeleIntake extends Subsystem implements GamepadListener {
     }
   }
 
-  private StateMachine buildStateMachine() {
-    StateMachine stateMachine = new StateMachine();
+  private StateMachine<MyState, Transition> buildStateMachine() {
+    StateMachine<MyState, Transition> stateMachine = new StateMachine<>();
 
     stateMachine
         .state(
-            new State(MyState.OFF)
+            new State<MyState, Transition>(MyState.OFF)
                 .on(Transition.SPIN_IN, MyState.SPINNING_IN)
                 .on(Transition.SPIN_OUT, MyState.SPINNING_OUT)
         )
         .state(
-            new State(MyState.SPINNING_IN)
+            new State<MyState, Transition>(MyState.SPINNING_IN)
                 .on(Transition.TURN_OFF, MyState.OFF)
                 .on(Transition.SPIN_OUT, MyState.SPINNING_OUT)
         )
         .state(
-            new State(MyState.SPINNING_OUT)
+            new State<MyState, Transition>(MyState.SPINNING_OUT)
                 .on(Transition.TURN_OFF, MyState.OFF)
                 .on(Transition.SPIN_IN, MyState.SPINNING_IN)
         );
