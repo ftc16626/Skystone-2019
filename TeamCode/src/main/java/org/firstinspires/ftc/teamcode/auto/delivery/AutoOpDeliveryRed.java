3da483c036d9912d3c8482f8c75ce806cdd7c064
Noah <noahbresler2002@gmail.com>
2020-01-03 06:12:33 -0600

    feat(AutoOpDeliveryRed): add starting position to auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/delivery/AutoOpDeliveryRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/delivery/AutoOpDeliveryRed.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.auto.delivery;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import org.firstinspires.ftc.teamcode.auto.RoadRunnerBaseOpmode;
 import org.firstinspires.ftc.teamcode.auto.SubsystemVision;
 import org.firstinspires.ftc.teamcode.subsystem.Subsystem;
@@ -10,6 +11,10 @@ public class AutoOpDeliveryRed extends RoadRunnerBaseOpmode {
 
   private SubsystemVision subsystemVision;
   private Subsystem pathLeft, pathMiddle, pathRight;
+
+  private double startX = -15;
+  private double startY = -63;
+  private double startHeading = Math.toRadians(90);
 
   public AutoOpDeliveryRed() {
     super();
@@ -53,6 +58,8 @@ public class AutoOpDeliveryRed extends RoadRunnerBaseOpmode {
     // ensure that path is only set once in case the vision thing changes
     // so you don't have multiple paths clashing
     if(pathSet) return;
+
+    this.drive.setPoseEstimate(new Pose2d(startX, startY, startHeading));
 
     switch(subsystemVision.pipeline.getDetectedSkystonePosition()) {
       case 0:
