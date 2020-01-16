0718ba2219b4271d3401eab90d81882defad93e7
Noah <noahbresler2002@gmail.com>
2020-01-16 10:53:37 -0600

    feat(SubsystemVirtual4Bar): add resting auto intake grabber and slow mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SubsystemTeleVirtual4Bar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SubsystemTeleVirtual4Bar.java
@@ -9,8 +9,12 @@ import org.firstinspires.ftc.teamcode.util.gamepadextended.listener.GamepadListe
 import org.firstinspires.ftc.teamcode.util.gamepadextended.listener.GamepadType;
 
 public class SubsystemTeleVirtual4Bar extends Subsystem implements GamepadListener {
+
   private final Robot robot;
   private final DriverInterface driverInterface;
+
+  private final double SPEED = 60;
+  private final double SPEED_SLOW = 120;
 
   public SubsystemTeleVirtual4Bar(Robot robot, DriverInterface driverInterface) {
     this.robot = robot;
@@ -28,22 +32,27 @@ public class SubsystemTeleVirtual4Bar extends Subsystem implements GamepadListen
   public void update() {
     double joystick = -driverInterface.aid.gamepad.right_stick_y;
 
-    if(joystick != 0) {
-      robot.subsystemVirtual4Bar.move(joystick / 30);
+    if (joystick != 0) {
+      if (driverInterface.aid.gamepad.right_trigger > 0.1) {
+        robot.subsystemVirtual4Bar.move(joystick / SPEED_SLOW);
+      } else {
+        robot.subsystemVirtual4Bar.move(joystick / SPEED);
+      }
     }
   }
 
   @Override
   public void actionPerformed(GamepadEventName eventName, GamepadEventType eventType,
       GamepadType gamepadType) {
-    if(gamepadType == GamepadType.AID && eventType == GamepadEventType.BUTTON_PRESSED) {
-      if(eventName == GamepadEventName.RIGHT_BUMPER) {
+    if (gamepadType == GamepadType.AID && eventType == GamepadEventType.BUTTON_PRESSED) {
+      if (eventName == GamepadEventName.RIGHT_BUMPER) {
         robot.subsystemVirtual4Bar.clamp();
-      } else if(eventName == GamepadEventName.LEFT_BUMPER) {
+      } else if (eventName == GamepadEventName.LEFT_BUMPER) {
         robot.subsystemVirtual4Bar.release();
+        robot.subsystemAutoIntakeGrabber.reset();
       }
 
-      if(eventName == GamepadEventName.B) {
+      if (eventName == GamepadEventName.A) {
         robot.subsystemVirtual4Bar.flipSide();
       }
     }
