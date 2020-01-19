0a15995972633a4695bbef2a926517e975e8ab49
Noah <noahbresler2002@gmail.com>
2020-01-19 00:25:42 -0600

    feat(DriveConstants): update drive constants and rip out the old gobilda custom motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/roadrunner/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/roadrunner/DriveConstants.java
@@ -3,27 +3,31 @@ package org.firstinspires.ftc.teamcode.hardware.roadrunner;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints;
+import com.qualcomm.hardware.motors.GoBILDA5202Series;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
-import org.firstinspires.ftc.teamcode.hardware.util.GoBILDA5202Series435;
+//import org.firstinspires.ftc.teamcode.hardware.util.GoBILDA5202Series435;
 
 @Config
 public class DriveConstants {
 
   private static final MotorConfigurationType MOTOR_CONFIG = MotorConfigurationType.getMotorType(
-      GoBILDA5202Series435.class);
+      GoBILDA5202Series.class);
+
+  private static final double TICKS_PER_REV = 386.3;
+  private static final double MAX_RPM = 435;
 
   public static final boolean RUN_USING_ENCODER = true;
 
-  public static double kP = 20;
-  public static double kI = 4;
-  public static double kD = 12;
+  public static double kP = 28.5;
+  public static double kI = 0;
+  public static double kD = 17.5;
 
   public static final PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(kP, kI, kD);
 
-  public static double WHEEL_RADIUS = 1.9685;
+  public static double WHEEL_RADIUS = 1.9685 * 0.972 * 1.006944;
   // output (wheel) speed / input (motor) speed
   public static double GEAR_RATIO = 0.5;
-  public static double TRACK_WIDTH = 15.1181;
+  public static double TRACK_WIDTH = 15.06;
 
   public static double kV = 1.0 / rpmToVelocity(getMaxRpm());
   public static double kA = 0;
@@ -35,7 +39,8 @@ public class DriveConstants {
   );
 
   public static double encoderTicksToInches(double ticks) {
-    return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks /  MOTOR_CONFIG.getTicksPerRev();
+    return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+//    return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks /  MOTOR_CONFIG.getTicksPerRev();
   }
 
   public static double rpmToVelocity(double rpm) {
@@ -43,13 +48,16 @@ public class DriveConstants {
   }
 
   public static double getMaxRpm() {
-    return MOTOR_CONFIG.getMaxRPM() *
+//    return MOTOR_CONFIG.getMaxRPM() *
+//        (RUN_USING_ENCODER ? MOTOR_CONFIG.getAchieveableMaxRPMFraction() : 1.0);
+    return MAX_RPM *
         (RUN_USING_ENCODER ? MOTOR_CONFIG.getAchieveableMaxRPMFraction() : 1.0);
   }
 
   public static double getTicksPerSec() {
     // note: MotorConfigurationType#getAchieveableMaxTicksPerSecond() isn't quite what we want
-    return (MOTOR_CONFIG.getMaxRPM() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
+    return (MAX_RPM * TICKS_PER_REV / 60.0);
+//    return (MOTOR_CONFIG.getMaxRPM() * MOTOR_CONFIG.getTicksPerRev() / 60.0);
   }
 
   public static double getMotorVelocityF() {
