f0b20ea26986534b75900d45db452d3e15bcae92
Noah <noahbresler2002@gmail.com>
2020-01-28 10:35:34 -0600

    feat(SubsystemAutoIntakeGrabber): add separate mode for capstone grabbing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/SubsystemAutoIntakeGrabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/SubsystemAutoIntakeGrabber.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import android.util.Log;
 import com.ftc16626.missioncontrol.util.statemachine.State;
 import com.ftc16626.missioncontrol.util.statemachine.StateMachine;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
@@ -11,20 +12,27 @@ import org.openftc.revextensions2.RevBulkData;
 
 public class SubsystemAutoIntakeGrabber extends HardwareSubsystem {
 
-  private StateMachine<MyState, Transition> stateMachine = buildStateMachine();
+  private StateMachine<MyStateTeleop, TransitionTeleop> stateMachineTeleop = buildStateMachineTeleop();
+  private StateMachine<MyStateEngame, TransitionEngame> stateMachineEndgame = buildStateMachineEndgame();
 
   private final DigitalChannel frontBeamBreak, backSwitch;
   private final String[] switchIds = new String[]{
       "intakeBeamBreak", "intakeSwitch"
   };
 
-  ElapsedTime grabbedBlockTime = new ElapsedTime();
-
-  private final double IDLE_GRABBLOCK_TRANSITION_TIME = 0.3;
-  private final double ENDGAME_TRANSITION_TIME = 0.7;
-  private boolean ENDGAME_TOGGLED = false;
-
   private final double LIFTED_POSITION = 0.2;
+
+  ElapsedTime grabbedBlockTime = new ElapsedTime();
+  private final double IDLE_GRABBLOCK_TRANSITION_TIME = 0.3;
+
+  ElapsedTime capstoneElapsedTime = new ElapsedTime();
+  ElapsedTime initialLiftElapsed = new ElapsedTime();
+  ElapsedTime liftGrabElapsed = new ElapsedTime();
+  private boolean ENDGAME_TOGGLED = false;
+  private final double ENDGAME_CAPSTONE_SET_TIME = 0.7;
+  private final double ENDGAME_LIFT_WAIT = 0.5;
+  private final double ENDGAME_LIFT_LOWER = 0.2;
+  private final double ENDGAME_LIFT_CLAMP = ENDGAME_LIFT_LOWER + 0.1;
 
   public SubsystemAutoIntakeGrabber(Robot robot, RadicalOpMode opMode) {
     super(robot, opMode);
@@ -35,59 +43,163 @@ public class SubsystemAutoIntakeGrabber extends HardwareSubsystem {
 
   @Override
   public void update() {
+
+    if (!ENDGAME_TOGGLED) {
+      handleTeleop();
+    } else {
+      handleEndgame();
+    }
+  }
+
+  private void handleTeleop() {
     RevBulkData bulkData = getRobot().getBulkDataRight();
 
-    switch (Objects.requireNonNull(stateMachine.getCurrentState())) {
+    if(bulkData == null) return;
+
+    switch (Objects.requireNonNull(stateMachineTeleop.getCurrentState())) {
       case IDLE:
         if (!bulkData.getDigitalInputState(backSwitch)) {
-//          getRobot().subsystemAutoCapstone.lower();
-          stateMachine.transition();
           grabbedBlockTime.reset();
+          stateMachineTeleop.transition();
         }
         break;
       case GRABBED_BLOCK:
         getRobot().subsystemVirtual4Bar.clamp();
         if (grabbedBlockTime.seconds() > IDLE_GRABBLOCK_TRANSITION_TIME) {
-//        if (grabbedBlockTime.seconds() > (ENDGAME_TOGGLED ? ENDGAME_TRANSITION_TIME : IDLE_GRABBLOCK_TRANSITION_TIME)) {
-          stateMachine.transition();
+          stateMachineTeleop.transition();
         }
         break;
       case LIFTED_BLOCK:
         getRobot().subsystemVirtual4Bar.setPosition(LIFTED_POSITION);
-//        stateMachine.transition(Transition.RESET);
-        stateMachine.transition();
+        stateMachineTeleop.transition();
         break;
     }
   }
 
-  public void reset() {
-    if (stateMachine.getCurrentState() == MyState.TO_BE_RESET)
-      stateMachine.transition(Transition.RESET);
+  private void handleEndgame() {
+    RevBulkData bulkData = getRobot().getBulkDataRight();
+
+    if(bulkData == null) return;
+
+    Log.i("STATE", Objects.requireNonNull(stateMachineEndgame.getCurrentState()).toString());
+
+    switch (Objects.requireNonNull(stateMachineEndgame.getCurrentState())) {
+      case IDLE:
+        if (!bulkData.getDigitalInputState(backSwitch)) {
+          getRobot().subsystemVirtual4Bar.setPosition(LIFTED_POSITION);
+          initialLiftElapsed.reset();
+          stateMachineEndgame.transition();
+        }
+        break;
+      case LIFTING_ARM:
+        if (initialLiftElapsed.seconds() > ENDGAME_LIFT_WAIT) {
+          capstoneElapsedTime.reset();
+          stateMachineEndgame.transition();
+        }
+        break;
+      case SET_CAPSTONE:
+        getRobot().subsystemAutoCapstone.lower();
+        if (capstoneElapsedTime.seconds() > ENDGAME_CAPSTONE_SET_TIME) {
+          liftGrabElapsed.reset();
+          stateMachineEndgame.transition();
+        }
+        break;
+      case GRABBED_BLOCK:
+        getRobot().subsystemVirtual4Bar.setPosition(0);
+        if (liftGrabElapsed.seconds() > ENDGAME_LIFT_LOWER) {
+          getRobot().subsystemVirtual4Bar.clamp();
+        }
+
+        if (liftGrabElapsed.seconds() > ENDGAME_LIFT_CLAMP) {
+          stateMachineEndgame.transition();
+        }
+        break;
+      case LIFTED_BLOCK:
+        getRobot().subsystemVirtual4Bar.setPosition(LIFTED_POSITION);
+        stateMachineEndgame.transition();
+        break;
+    }
   }
 
-  private StateMachine<MyState, Transition> buildStateMachine() {
-    StateMachine<MyState, Transition> stateMachine = new StateMachine<>();
+  public void setEndgame(boolean set) {
+    ENDGAME_TOGGLED = set;
+
+//    Log.i("STATE6", Objects.requireNonNull(stateMachineEndgame.getCurrentState()).toString());
+
+//    if (ENDGAME_TOGGLED && stateMachineEndgame.getCurrentState() != MyStateEngame.IDLE) {
+//      stateMachineEndgame.transition(TransitionEngame.RESET);
+//    }
+    stateMachineEndgame.manualSet(MyStateEngame.IDLE);
+  }
+
+  public void reset() {
+    if (stateMachineTeleop.getCurrentState() == MyStateTeleop.TO_BE_RESET) {
+      stateMachineTeleop.transition(TransitionTeleop.RESET);
+      stateMachineEndgame.transition(TransitionEngame.RESET);
+    }
+  }
+
+  private StateMachine<MyStateTeleop, TransitionTeleop> buildStateMachineTeleop() {
+    StateMachine<MyStateTeleop, TransitionTeleop> stateMachine = new StateMachine<>();
 
     stateMachine
-        .state(new State<>(MyState.IDLE))
-        .state(new State<>(MyState.GRABBED_BLOCK))
-        .state(new State<>(MyState.LIFTED_BLOCK))
+        .state(new State<>(MyStateTeleop.IDLE))
+        .state(new State<>(MyStateTeleop.GRABBED_BLOCK))
+        .state(new State<>(MyStateTeleop.LIFTED_BLOCK))
         .state(
-            new State<MyState, Transition>(MyState.TO_BE_RESET)
-                .on(Transition.RESET, MyState.IDLE)
+            new State<MyStateTeleop, TransitionTeleop>(MyStateTeleop.TO_BE_RESET)
+                .on(TransitionTeleop.RESET, MyStateTeleop.IDLE)
         );
 
     return stateMachine;
   }
 
-  enum MyState {
+  private StateMachine<MyStateEngame, TransitionEngame> buildStateMachineEndgame() {
+    StateMachine<MyStateEngame, TransitionEngame> stateMachine = new StateMachine<>();
+
+    stateMachine
+        .state(new State<>(MyStateEngame.IDLE))
+        .state(new State<MyStateEngame, TransitionEngame>(MyStateEngame.LIFTING_ARM)
+//            .on(TransitionEngame.RESET, MyStateEngame.IDLE)
+        )
+        .state(new State<MyStateEngame, TransitionEngame>(MyStateEngame.SET_CAPSTONE)
+//            .on(TransitionEngame.RESET, MyStateEngame.IDLE)
+        )
+        .state(new State<MyStateEngame, TransitionEngame>(MyStateEngame.GRABBED_BLOCK)
+//            .on(TransitionEngame.RESET, MyStateEngame.IDLE)
+        )
+        .state(new State<MyStateEngame, TransitionEngame>(MyStateEngame.LIFTED_BLOCK)
+//            .on(TransitionEngame.RESET, MyStateEngame.IDLE)
+        )
+        .state(
+            new State<MyStateEngame, TransitionEngame>(MyStateEngame.TO_BE_RESET)
+                .on(TransitionEngame.RESET, MyStateEngame.IDLE)
+        );
+
+    return stateMachine;
+  }
+
+  enum MyStateTeleop {
     IDLE,
     GRABBED_BLOCK,
     LIFTED_BLOCK,
     TO_BE_RESET
   }
 
-  enum Transition {
+  enum TransitionTeleop {
+    RESET
+  }
+
+  enum MyStateEngame {
+    IDLE,
+    LIFTING_ARM,
+    SET_CAPSTONE,
+    GRABBED_BLOCK,
+    LIFTED_BLOCK,
+    TO_BE_RESET
+  }
+
+  enum TransitionEngame {
     RESET
   }
 }
